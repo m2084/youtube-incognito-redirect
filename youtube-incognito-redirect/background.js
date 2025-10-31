f27f76269709bd638031730aca2af17cfd5c2607
m2084 <116951478+m2084@users.noreply.github.com>
2025-10-31 13:01:17 -0300

    Add files via upload

---
+++ youtube-incognito-redirect/background.js
@@ -0,0 +1,27 @@
+﻿function shouldRedirect(url) {
+  return (
+    url.includes("www.youtube.com/watch") &&
+    !url.includes("&t=") &&
+    !url.includes("&list=")
+  );
+}
+
+async function openIncognito(url, tabId) {
+  chrome.windows.create({
+    url: url,
+    incognito: true,
+    focused: true
+  });
+
+  chrome.tabs.remove(tabId);
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (!changeInfo.url) return;
+
+  const url = changeInfo.url;
+
+  if (shouldRedirect(url)) {
+    openIncognito(url, tabId);
+  }
+});
